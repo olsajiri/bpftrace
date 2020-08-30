d03e11c4c1b1be19f5dc6c886698bd49a435e4a8
Jiri Olsa <jolsa@kernel.org>
2020-08-30 23:20:27 +0200

    Add dpath semantic_analyser test
    Adding semantic_analyser test.
    
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>

--- tests/mocks.h
+++ tests/mocks.h
@@ -68,6 +68,7 @@ public:
     has_override_return_ = std::make_optional<bool>(has_features);
     prog_kfunc_ = std::make_optional<bool>(has_features);
     has_loop_ = std::make_optional<bool>(has_features);
+    has_d_path_ = std::make_optional<bool>(has_features);
   };
 };
 
